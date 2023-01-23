6b7d13d0ad178b18d161481b78228767d449bfd7
VivianaS14 <vasorianp@gmail.com>
2023-01-23 17:58:12 -0500

    feat: firabase and app config

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDCfrVTe33a03sA1Pelp-cwNO9EbhIXdLk",
+    authDomain: "whatsapp-clone-e9a6a.firebaseapp.com",
+    projectId: "whatsapp-clone-e9a6a",
+    storageBucket: "whatsapp-clone-e9a6a.appspot.com",
+    messagingSenderId: "783755218034",
+    appId: "1:783755218034:web:8eba3623c281b4236986eb",
+    measurementId: "G-Z8QD9NBMTV"
+};
